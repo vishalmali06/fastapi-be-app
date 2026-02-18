96e09332d242b0dcbc77bb3b277183df3723bf50
Vishal Mali <vishalmali06@gmail.com>
2026-02-18 17:08:26 +0530

    why pydantic in python fastapi

---
+++ 1_pydantic_why.py
@@ -0,0 +1,41 @@
+from pydantic import BaseModel, EmailStr, AnyUrl, Field
+from typing import List, Dict, Optional, Annotated
+
+
+class Patient(BaseModel):
+    name: Annotated[str, Field(max_length=50, title='Name of the patient', description='Give the name of patient in less than 50 chars', examples=['Vishal', 'Amit'])]
+    email: EmailStr
+    linkedin_url : AnyUrl
+    age: int = Field(gt=0, lt=100)
+    weight: Annotated[float, Field(gt=0, strict=True)]
+    married: Annotated[bool, Field(default=None, description='Is the patient married or not')]
+    allergies: Annotated[Optional[List[str]], Field(default=None, max_length=5)]
+    contact_details: Dict[str, str]
+
+
+def insert_patient_data(patient: Patient):
+    print(patient.name)
+    print(patient.age)
+    print("insert")
+
+
+def update_patient_data(patient: Patient):
+    print(patient.name)
+    print(patient.age)
+    print(patient.allergies)
+    print(patient.married)
+    print("update")
+
+
+patient_info = {
+    "name": "Vishal",
+    "email": "vishal@gmail.com",
+    "linkedin_url" : 'https://www.linkedin.com/in/vishal-mali/',
+    "age": 30,   # better to pass int instead of string
+    "weight": 75.2,
+    "contact_details": {"phone": "2233445566"},
+}
+
+patient1 = Patient(**patient_info)
+
+update_patient_data(patient1)
